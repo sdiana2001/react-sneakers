83b727ddc3920194778070b0580e10cbdbbafaca
Sogdiana <sogdianaakmatova4@gmail.com>
2024-08-27 03:19:58 +0600

    Добавлены функции для сброса корзины итп

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Product from './components/Product';
 import Drawer from './components/Drawer/Drawer';
 import Header from './components/Header/Header';
@@ -7,50 +8,70 @@ function App() {
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [cartOpened, setCartOpened] = useState(false);
+  const [searchValue, setsearchValue] = useState('');
 
   useEffect(() => {
-    fetch('https://66a904f6e40d3aa6ff5a4dc3.mockapi.io/items')
-      .then((response) => response.json())
-      .then((json) => setItems(json));
+      axios.get('https://66a904f6e40d3aa6ff5a4dc3.mockapi.io/items').then((res) =>{
+        setItems(res.data);
+      })
+     
+      axios.get('https://66a904f6e40d3aa6ff5a4dc3.mockapi.io/cart').then((res) =>{
+        setCartItems(res.data);
+      })
   }, []);
 
-  
 
-  const onAddToCart = (obj) => {
-    // Проверяем, есть ли товар уже в корзине
-    const isItemInCart = cartItems.some(item => item.id === obj.id);
+//функция добавления в корзину
+    const onAddToCart = (obj) => {
+    const isItemInCart = cartItems.some(item => item.id === obj.id);  // Если в cartItems(корзине) есть товар с таким же id, как у объекта obj(доб-емой карты), то возвращается true. 
 
     if (isItemInCart) {
       setCartItems(prev => prev.filter(item => item.id !== obj.id));   // Если товар уже есть в корзине, удаляем его
-    } else {
+    //Метод filter который создает новый массив, включающий только те элементы, которые удовлетворяют условию.
+    }
+     else {
       setCartItems(prev => [...prev, obj]);  // Если товара нет в корзине, добавляем его
     }
+
+    axios.post('https://66a904f6e40d3aa6ff5a4dc3.mockapi.io/cart', obj);
+
   };
+
+  const onRemoveItem = (id)=>{
+    setCartItems((prev) => prev.filter(item => item.id !== id));  
+  }
+
+  const onChangeSearchInput = (event)=> {
+    setsearchValue(event.target.value);
+  }
 
 
   return (
     <div className="wrapper clear">
-      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove ={onRemoveItem} />}
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex justify-between align-center mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
           <div className="search-block">
             <img src="/img/icons/search.svg" alt="search" />
-            <input placeholder="Поиск..." />
+            {searchValue && <img className='cu-p clear' onClick={()=> setsearchValue('')} src="/img/icons/remove__btn.svg" alt="Close" />}
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..." />
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {items.map((item) => (
-            <Product
-              key={item.id}
-              img={item.img}
-              title={item.title}
-              price={item.price}
-              onFavorite={() => console.log('Нажали на сердце')}
-              onPlus={() => onAddToCart(item)} // Передаем весь объект товара
-            />
-          ))}
+        {items.filter(item => item.title && item.title.toLowerCase()
+        .includes(searchValue.toLowerCase()))
+        .map((item) => (
+        <Product
+          key={item.id}
+          img={item.img}
+          title={item.title}
+          price={item.price}
+          onFavorite={() => console.log('Нажали на сердце')}
+          onPlus={() => onAddToCart(item)} // Передаем весь объект товара
+        />
+      ))}
         </div>
       </div>
     </div>
--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,12 +1,22 @@
 import  '../../index.scss';
 
-function Drawer({onClose,  items = []}) {
+function Drawer({onClose, onRemove,  items = []}) {
     return (
     <div className="overlay">
     <div className="drawer">
     <h2 className='mb-30' d-flex align-center justify-between >
       Корзина <img onClick={onClose} src="/img/icons/remove__btn.svg" alt="Remove" />
       </h2>
+
+      <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+      <img className="mb-20" width="120px" src="/img/icons/empty-cart.svg" alt="Empty" />
+      <h2>Корзина пустая</h2>
+      <p className="opacity-6">Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
+      <button className="greenButton">
+        <img src="/img/icons/arrow-btn.svg" alt="Arrow" />
+        Вернуться назад
+      </button>
+    </div>
 
     <div className="items">
       {items.map((obj) =>(
@@ -17,12 +27,11 @@ function Drawer({onClose,  items = []}) {
       <p className="mb-5">{obj.title}</p>
       <b>{obj.price}</b>
       </div>
-      <img className='removeBtn' src="/img/icons/remove__btn.svg" alt="" />
+      <img className='removeBtn' onClick={() => onRemove(obj.id)} src="/img/icons/remove__btn.svg" alt="" />
       </div>
       ))}
-    </div>
-
-    <div className="cartTotalblock">
+      </div>
+      <div className="cartTotalblock">
       <ul>
         <li>
           <span>Итого: </span>
